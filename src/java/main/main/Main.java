f8763f1c8459d08deee372aa5a11090a5743d276
kosta <kingpin333@i.ua>
2014-06-17 21:45:57 +0300

    first commit

---
+++ src/java/main/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.util.logging.Logger;
+
+import test.Trash;
+
+public class Main {
+    private static final Logger LOG = Logger.getLogger(Main.class.getName());
+    private Main() {
+    }
+    public static void main(String[] args) {
+        new Trash().setName("vasya");
+        LOG.info("Sonar test");
+    }
+}
---
+++ src/java/main/test/Trash.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Trash {
+    private String name;
+
+    public Trash() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
