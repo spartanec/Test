26d441f948793c32f472717ba9e55445d9d4752f
kosta <kingpin333@i.ua>
2014-06-19 21:51:31 +0300

    Added sonar analyse

--- src/java/main/main/Main.java
+++ src/java/main/main/Main.java
@@ -2,15 +2,12 @@ package main;
 
 import java.util.logging.Logger;
 
-import test.Trash;
-
 public class Main {
     private static final Logger LOG = Logger.getLogger(Main.class.getName());
     private Main() {
     }
     public static void main(String[] args) {
-        new Trash().setName("vasya");
+        //new Trash().setName("vasya");
         LOG.info("Jenkins test number one ant two");
-        real error;
     }
 }
